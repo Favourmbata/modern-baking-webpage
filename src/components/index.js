5c03f2c8cd6310b9a30d5b4bc99cc70d58608f75
Favourmbata <134517996+Favourmbata@users.noreply.github.com>
2024-09-19 03:43:53 +0100

    my bankin web page

---
+++ src/components/index.js
@@ -0,0 +1,23 @@
+import Navbar from './Navbar'
+import Hero from './Hero'
+import Stats from './Stats'
+import Business from './Business'
+import Billing from './Billing'
+import CardDeal from './CardDeal'
+import Testimonials from './Testimonials'
+import Footer from './Footer'
+import CTA from './CTA'
+import Clients from './Clients'
+
+export{
+    Navbar,
+    Hero,
+    Stats,
+    CTA,
+    CardDeal,
+    Footer,
+    Testimonials,
+    Clients,
+    Billing,
+    Business,
+}
